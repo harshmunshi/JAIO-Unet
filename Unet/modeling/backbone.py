928c6bddca4f175cdd200c3d4d9b00be26c430bf
Harsh Hemangkumar Munshi <harsh.hemangkumar.munshi@iais.fraunhofer.de>
2020-09-20 01:39:57 +0200

    added decoder stream

--- Unet/modeling/backbone.py
+++ Unet/modeling/backbone.py
@@ -28,15 +28,15 @@ class Encoder(nn.Module):
     """
     Encoder of the U-Net arch, used to capture the context
     """
-    def __init__(self, in_planes, out_planes, depth=5):
+    def __init__(self, in_planes, out_planes):
         super(Encoder, self).__init__()
         # define the network here
         # The first down conv block
-        self.dconv1 = DownConvBlock(in_planes, out_planes*2**0, stride=1, padding=1, kernel_size=3) #d=64
-        self.dconv2 = DownConvBlock(out_planes, out_planes*2**1, stride=1, padding=1, kernel_size=3) #d=128
-        self.dconv3 = DownConvBlock(out_planes*2, out_planes*2**2, stride=1, padding=1, kernel_size=3) #d=256
-        self.dconv4 = DownConvBlock(out_planes*4, out_planes*2**3, stride=1, padding=1, kernel_size=3) #d=512
-        self.dconv5 = DownConvBlock(out_planes*8, out_planes*2**4, stride=1, padding=1, kernel_size=3) #d=1024
+        self.dconv1 = DownConvBlock(in_planes, out_planes, stride=1, padding=1, kernel_size=3) #d=64
+        self.dconv2 = DownConvBlock(out_planes, out_planes*2, stride=1, padding=1, kernel_size=3) #d=128
+        self.dconv3 = DownConvBlock(out_planes*2, out_planes*4, stride=1, padding=1, kernel_size=3) #d=256
+        self.dconv4 = DownConvBlock(out_planes*4, out_planes*8, stride=1, padding=1, kernel_size=3) #d=512
+        self.dconv5 = DownConvBlock(out_planes*8, out_planes*16, stride=1, padding=1, kernel_size=3) #d=1024
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
     
     def forward(self, inputs):
@@ -57,5 +57,34 @@ class Encoder(nn.Module):
 
 
 class Decoder(nn.Module):
-    def __init__(self, in_planes, out_planes):
-        raise NotImplementedError()
+    def __init__(self, in_planes, out_planes, encoder_in, encoder_out, n_classes):
+        # in case of the decoder the input will be the last layer
+        self.encoder = Encoder(encoder_in, encoder_out)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.uconv1 = DownConvBlock(out_planes*16, out_planes*8, stride=1, padding=1, kernel_size=3) #1024->512
+        self.uconv2 = DownConvBlock(out_planes*8, out_planes*4, stride=1, padding=1, kernel_size=3) #512 -> 256
+        self.uconv3 = DownConvBlock(out_planes*4, out_planes*2, stride=1, padding=1, kernel_size=3) #256 -> 128
+        self.uconv4 = DownConvBlock(out_planes*2, out_planes, stride=1, padding=1, kernel_size=3) # 128 -> 64
+
+        # The output map is a 1x1 convolutional layer 
+        self.output_map = nn.Conv2D(out_planes, n_classes, kernel_size=1)
+    
+    def forward(self, x):
+        # c1 is out_dconv1, c2 is out_dconv2.... etc
+        c1, c2, c3, c4, c5 = self.encoder(x)
+
+        # upsample c5 and add to c4
+        in_1 = torch.cat([c4, self.up(c5)], dim=1)
+        x = self.uconv1(in_1)
+
+        in_2 = torch.cat([c3, self.up(x)], dim=1)
+        x = self.uconv2(in_2)
+
+        in_3 = torch.cat([c2, self.up(x)], dim=1)
+        x = self.uconv3(in_3)
+
+        int_4 = torch.cat([c1, self.up(x)], dim=1)
+        x = self.uconv4(in_4)
+
+        x = self.output_map(x)
+        return x
