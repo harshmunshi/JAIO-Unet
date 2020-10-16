76817fdcc7cee9a33f88a3b74834feef702d5431
Harsh Hemangkumar Munshi <harsh.hemangkumar.munshi@iais.fraunhofer.de>
2020-10-16 14:34:28 +0200

    Pascal VOC dataset bare minimum

---
+++ Unet/data/data_loader.py
@@ -0,0 +1,158 @@
+import torch
+import numpy as np
+from os.path import join
+import os, sys
+from PIL import Image
+import collections
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+# In order to use the cross entropy loss, we need the target vector in the form of N dim vector
+
+# define a palatte for converting the mask into one hot encoded map
+
+# palette = {
+#     'background':[0, 0, 0],
+#     'aeroplane':[128, 0, 0],
+#     'bicycle':[0, 128, 0],
+#     'bird':[128, 128, 0],
+#     'boat':[0, 0, 128],
+#     'bottle':[128, 0, 128],
+#     'bus':[0, 128, 128],
+#     'car':[128, 128, 128],
+#     'cat':[64, 0, 0],
+#     'chair':[192, 0, 0],
+#     'cow':[64, 128, 0],
+#     'diningtable':[192, 128, 0],
+#     'dog':[64, 0, 128],
+#     'horse':[192, 0, 128],
+#     'motorbike':[64, 128, 128],
+#     'person':[192, 128, 128],
+#     'pottedplant':[0, 64, 0],
+#     'sheep':[128, 64, 0],
+#     'sofa':[0, 192, 0],
+#     'train':[128, 192, 0],
+#     'tvmonitor':[0, 64, 128],
+#     'void':[224, 224, 192]}
+
+palette = [[0, 0, 0],
+           [128, 0, 0],
+           [0, 128, 0],
+           [128, 128, 0],
+           [0, 0, 128],
+           [128, 0, 128],
+           [0, 128, 128],
+           [128, 128, 128],
+           [64, 0, 0],
+           [192, 0, 0],
+           [64, 128, 0],
+           [192, 128, 0],
+           [64, 0, 128],
+           [192, 0, 128],
+           [64, 128, 128],
+           [192, 128, 128],
+           [0, 64, 0],
+           [128, 64, 0],
+           [0, 192, 0],
+           [128, 192, 0],
+           [0, 64, 128],
+           [224, 224, 192]]
+
+
+class VOCLoader(Dataset):
+    """
+    The ground truth is given in the form of RGB images.
+    So we approach the target creation as following steps:
+    1. Normalize the dataset (for faster convergence, [see batch normalization]).
+    2. Normalize the image.
+    3. Convert the mask from RGB to label_mask format.
+    4. In __getitem__ return normalized image as image and label_mask for that image as the target
+    """
+    def __init__(self, root_dir, test_mode=False, augmentations=None):
+        super(VOCLoader, self).__init__()
+        # read the images as well as the labels, convert the labels in one hot format
+        # This is the root dir <VOC_ROOT/VOC2012>
+        self.root_dir = root_dir
+        self.test_mode = test_mode
+        self.n_class = 21
+        self.augmentations = augmentations
+        #self.mean = np.array([104.00699, 116.66877, 122.67892])
+        self.files = collections.defaultdict()
+        # join the paths <ImageSets/Segmentation> to the root directory
+        if not self.test_mode:
+            for split in ["train", "val", "trainval"]:
+                path = join(self.root, "ImageSets/Segmentation", split + ".txt")
+                file_list = tuple(open(path, "r"))
+                file_list = [id_.strip() for id_ in file_list]
+                self.files[split] = file_list
+        
+        # normalization for image (in order to get better gradient flow towards the minima)
+        self.tf = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+
+            ]
+        )
+    
+
+    def encode_mask(self, mask):
+        """
+        Encode the label masks from RGB to class one hot encoding
+        """
+
+        mask = mask.astype(int)
+        label_mask = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.int16)
+        for idx, label in enumerate(palette):
+            # get the indices and start filling the blank image based on the RGB color from the palette
+            label_mask[np.where(np.all(mask == label, axis=-1))[:2]] = idx
+        label_mask = label_mask.astype(int)
+        return label_mask
+
+
+    def preprocess(self, name):
+        # name is the path to that image and the label
+        label_path = join(root_dir, SegmentationClass)
+        img_path = join(root_dir, JPEFImages)
+        img = Image.open(img_path).convert("RGB")
+        mask = Image.open(label_path).convert("RGB")
+
+        # transform the image and encode the mask
+        # NOTE: Ideally at this point we have to resize the input image but
+        # since the input and output dimensions are same for the respective masks, it doesn't matter
+        img = self.tf(img)
+        mask = encode_mask(mask)
+
+        # convert the mask to a torch tensor
+        mask = torch.from_array(mask).long()
+        mask[mask == 255] = 0
+        return img, mask
+    
+    
+    def __getitem__(self, idx):
+        """
+        fetch the image and annotation by index
+        """
+        if not self.test_mode: 
+            # If it's training
+            #index = np.random.choice(len(self.files["train"]), 1)[0]
+            name = self.files["train"][idx]
+        elif self.test_mode==True:
+            #index = np.random.choice(len(self.files["val"]), 1)[0]
+            name = self.files["val"][idx]
+        
+        image, mask = preprocess(name)
+        return image, mask        
+
+# load a sample image
+fixed_path = join()
+sample_img_path = fixed_path + "2010_005830.png"
+img = Image.open(sample_img_path).convert('RGB')
+to_mask(img)
+
+def unit_test():
+    raise NotImplementedError()
+
+if __name__=="__main__":
+    unit_test()
+    print("============Unit Test Passed==============")
--- Unet/modeling/backbone.py
+++ Unet/modeling/backbone.py
@@ -1,90 +1,13 @@
-import torch
 import torch.nn as nn
-from torch.autograd import Variable
-import numpy as np
 
-class DownConvBlock(nn.Module):
-    """
-    NOTE: This is for the down block including the double conv
-    """
-    def __init__(self, in_planes, out_planes, stride, padding, kernel_size):
-        self.conv1 = nn.Conv2D(in_planes, out_planes, kernel_size=kernel_size, padding=padding, stride=stride)
-        self.relu = nn.ReLU(inplace=True)
-        self.bn1 = nn.BatchNorm2d(out_planes)
-        self.conv2 = nn.Conv2D(out_planes, out_planes, kernel_size=kernel_size, padding=padding, stride=stride)
-        self.bn2 = nn.BatchNorm2d(out_planes)
-        # no nonlinearity after this point
-    
-    def forward(self,inputs):
-        out = self.conv1(inputs)
-        out = self.bn1(out)
-        out = self.relu(out)
-        out = self.conv2(out)
-        out = self.bn2(out)
+"""
+Encoder Decoder Architecture
+"""
 
-        return out
-
-class Encoder(nn.Module):
-    """
-    Encoder of the U-Net arch, used to capture the context
-    """
+class DownConv(nn.Module):
     def __init__(self, in_planes, out_planes):
-        super(Encoder, self).__init__()
-        # define the network here
-        # The first down conv block
-        self.dconv1 = DownConvBlock(in_planes, out_planes, stride=1, padding=1, kernel_size=3) #d=64
-        self.dconv2 = DownConvBlock(out_planes, out_planes*2, stride=1, padding=1, kernel_size=3) #d=128
-        self.dconv3 = DownConvBlock(out_planes*2, out_planes*4, stride=1, padding=1, kernel_size=3) #d=256
-        self.dconv4 = DownConvBlock(out_planes*4, out_planes*8, stride=1, padding=1, kernel_size=3) #d=512
-        self.dconv5 = DownConvBlock(out_planes*8, out_planes*16, stride=1, padding=1, kernel_size=3) #d=1024
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-    
-    def forward(self, inputs):
-        out_dconv1 = self.dconv1(inputs)
-        out_dconv1_mp = self.maxpool(out_dconv1)
-
-        out_dconv2 = self.dconv2(out_dconv1_mp)
-        out_dconv2_mp = self.maxpool(out_dconv2)
-
-        out_dconv3 = self.dconv3(out_dconv2_mp)
-        out_dconv3_mp = self.maxpool(out_dconv3)
-        
-        out_dconv4 = self.dconv4(out_dconv3_mp)
-        out_dconv4_mp = self.maxpool(out_dconv4)
-
-        out_dconv5 = self.dconv5(out_dconv4_mp)
-        return [out_dconv1, out_dconv2, out_dconv3, out_dconv4, out_dconv5]
-
-
-class Decoder(nn.Module):
-    def __init__(self, in_planes, out_planes, encoder_in, encoder_out, n_classes):
-        # in case of the decoder the input will be the last layer
-        self.encoder = Encoder(encoder_in, encoder_out)
-        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.uconv1 = DownConvBlock(out_planes*16, out_planes*8, stride=1, padding=1, kernel_size=3) #1024->512
-        self.uconv2 = DownConvBlock(out_planes*8, out_planes*4, stride=1, padding=1, kernel_size=3) #512 -> 256
-        self.uconv3 = DownConvBlock(out_planes*4, out_planes*2, stride=1, padding=1, kernel_size=3) #256 -> 128
-        self.uconv4 = DownConvBlock(out_planes*2, out_planes, stride=1, padding=1, kernel_size=3) # 128 -> 64
-
-        # The output map is a 1x1 convolutional layer 
-        self.output_map = nn.Conv2D(out_planes, n_classes, kernel_size=1)
-    
-    def forward(self, x):
-        # c1 is out_dconv1, c2 is out_dconv2.... etc
-        c1, c2, c3, c4, c5 = self.encoder(x)
-
-        # upsample c5 and add to c4
-        in_1 = torch.cat([c4, self.up(c5)], dim=1)
-        x = self.uconv1(in_1)
-
-        in_2 = torch.cat([c3, self.up(x)], dim=1)
-        x = self.uconv2(in_2)
-
-        in_3 = torch.cat([c2, self.up(x)], dim=1)
-        x = self.uconv3(in_3)
-
-        int_4 = torch.cat([c1, self.up(x)], dim=1)
-        x = self.uconv4(in_4)
-
-        x = self.output_map(x)
-        return x
+        super(DownConv, self).__init__()
+        """
+        Down convolution block, resembling Unet Architecture
+        """
+        self.downblock = nn.Sequential()
